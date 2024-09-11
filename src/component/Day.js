cb2d65725a5003851aa0c950fd3ccd4250d04efd
leejaehong <woqhr53@naver.com>
2024-09-11 21:19:59 +0900

    커밋

---
+++ src/component/Day.js
@@ -0,0 +1,22 @@
+
+import { useParams } from "react-router-dom";
+import Word from "./Word";
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
+export default function Day(){
+
+    const day = useParams().day;
+    const words = useFetch(`http://localhost:3001/words?day=${day}`);
+    return(
+    <>
+        <h2>Day {day}</h2>
+        <table>
+            <tbody>
+                {words.map(word =>(
+                    <Word word={word} key={word.id}/>
+                ))}
+            </tbody>
+        </table>
+    </>
+    );
+}
